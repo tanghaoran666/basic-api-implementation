9a1e19271032686b92717041ed31ad788f19c5a4
tanghaoran <407458281@qq.com>
2020-09-17 09:00:31 +0800

    implement logger

--- src/main/java/com/thoughtworks/rslist/component/RsEventHandler.java
+++ src/main/java/com/thoughtworks/rslist/component/RsEventHandler.java
@@ -1,9 +1,12 @@
 package com.thoughtworks.rslist.component;
 
+import com.thoughtworks.rslist.api.LoggingController;
 import com.thoughtworks.rslist.exception.Error;
 import com.thoughtworks.rslist.exception.RsEventNotValidException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,11 +15,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class RsEventHandler {
     @ExceptionHandler({RsEventNotValidException.class})
     private ResponseEntity rsExceptionHandle(Exception e) {
+        Logger logger = LogManager.getLogger(LoggingController.class);
         Error error = new Error();
-
-
+        logger.catching(Level.ERROR,e);
         error.setError(e.getMessage());
-
         return ResponseEntity.badRequest().body(error);
 
     }
