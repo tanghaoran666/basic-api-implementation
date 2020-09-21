ae29af1f4643161282ca0932df6099aeabe6b10f
tanghaoran <407458281@qq.com>
2020-09-21 12:26:45 +0800

    reafactory service and controller without @Autowired

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -32,134 +32,55 @@ import java.util.stream.Collectors;
 
 public class RsController {
 
-  @Autowired
-  RsService rsService;
-  @Autowired
-  RsEventRepository rsEventRepository;
-  @Autowired
-  UserRepository userRepository;
-  @Autowired
-  VoteRepository voteRepository;
+  final RsService rsService;
+
+  public RsController(RsService rsService){
+    this.rsService = rsService;
+  }
 
   @GetMapping("/rs/{id}")
   public ResponseEntity getOneList(@PathVariable int id){
-    Optional<RsEventPo> existPo = rsEventRepository.findById(id);
-    if(!existPo.isPresent()){
-      throw new RsEventNotValidException("invalid id");
-    }
-    RsEventPo rsEventPo = existPo.get();
-    RsEvent rsEvent = RsEvent.builder()
-            .userId(rsEventPo.getId())
-            .keyWord(rsEventPo.getKeyWord())
-            .eventName(rsEventPo.getEventName())
-            .rsEventId(rsEventPo.getId())
-            .voteNum(rsEventPo.getVoteNum())
-            .build();
+
+    RsEvent rsEvent = rsService.getEvent(id);
     return ResponseEntity.ok(rsEvent);
   }
 
   @GetMapping("/rs/list")
   public ResponseEntity getBetweenList(@RequestParam(required = false) Integer start,@RequestParam(required = false) Integer end){
-    List<RsEvent> rsEvents = rsEventRepository.findAll().stream().map(
-            item -> RsEvent.builder().keyWord(item.getKeyWord())
-                    .eventName(item.getEventName())
-                    .userId(item.getUserPo().getId()).build()
-    ).collect(Collectors.toList());
-
-
-    if(start == null || end == null){
-      return ResponseEntity.ok(rsEvents);
-    }
-    if(start<=0 || end > rsEventRepository.findAll().size() || start>end){
-      throw new RsEventNotValidException("invalid request param");
-    }
-    return ResponseEntity.ok(rsEvents.subList(start-1 , end));
+    List<RsEvent> rsEvents = rsService.getEvents(start,end);
+    return ResponseEntity.ok(rsEvents);
   }
 
   @GetMapping("/users")
   public ResponseEntity getUserList(){
-    List<User> users = userRepository.findAll().stream().map(
-            item -> User.builder()
-                    .voteNumber(item.getVoteNumber())
-                    .phone(item.getPhone())
-                    .name(item.getName())
-                    .gender(item.getGender())
-                    .email(item.getEmail())
-                    .age(item.getAge())
-                    .build()
-    ).collect(Collectors.toList());
+
+    List<User> users = rsService.getUsers();
     return ResponseEntity.ok(users);
   }
 
   @PostMapping("/rs/event")
   public ResponseEntity postList(@RequestBody @Valid RsEvent rsEvent){
-    if(!userRepository.findById(rsEvent.getUserId()).isPresent()){
-      return ResponseEntity.badRequest().build();
-    }
-    UserPo userPo = userRepository.findById(rsEvent.getUserId()).get();
-    RsEventPo rsEventPo = RsEventPo
-                                    .builder()
-                                    .keyWord(rsEvent.getKeyWord())
-                                    .eventName(rsEvent.getEventName())
-                                    .userPo(userPo).build();
-    rsEventRepository.save(rsEventPo);
+    rsService.postEvent(rsEvent);
     return ResponseEntity.created(null).build();
   }
 
   @PatchMapping("/rs/{eventId}")
   public ResponseEntity patchListViaBody(@PathVariable int eventId,@RequestBody RsEvent rsEvent){
-    if(!rsEventRepository.findById(eventId).isPresent()){
-      return ResponseEntity.badRequest().build();
-    }
-    int userId = rsEvent.getUserId();
-    RsEventPo rsEventPo = rsEventRepository.findById(eventId).get();
-
-    if(userId!= rsEventPo.getUserPo().getId()){
-      return ResponseEntity.badRequest().build();
-    }
-    if(rsEvent.getKeyWord()!=null){
-      rsEventPo.setKeyWord(rsEvent.getKeyWord());
-    }
-    if(rsEvent.getEventName()!=null){
-      rsEventPo.setEventName(rsEvent.getEventName());
-    }
-    rsEventRepository.save(rsEventPo);
+    rsService.patchEvent(eventId,rsEvent);
     return ResponseEntity.ok(null);
   }
 
   @DeleteMapping("/rs/{id}")
   public ResponseEntity deleteList(@PathVariable int id){
 
-    rsEventRepository.deleteById(id);
+    rsService.deleteEvent(id);
     return ResponseEntity.ok(null);
   }
 
-  @DeleteMapping("/rs/reStart")
-  public ResponseEntity reStart(){
-    rsEventRepository.deleteAll();
-    return ResponseEntity.ok(null);
-  }
+
 
   @PostMapping("/rs/vote/{eventId}")
   public ResponseEntity voteRsEvent(@PathVariable int eventId, @RequestBody  Vote vote){
-//    vote.setRsEventId(eventId);
-//    vote.setLocalDateTime(LocalDateTime.now());
-//    int voteNum = vote.getVoteNum();
-//    UserPo userPo = userRepository.findById(vote.getUserId()).get();
-//    if(voteNum>userPo.getVoteNumber()){
-//      throw new RsEventNotValidException("invalid vote number");
-//    }
-//    RsEventPo rsEventPo = rsEventRepository.findById(eventId).get();
-//    userPo.setVoteNumber(userPo.getVoteNumber() - voteNum);
-//    rsEventPo.setVoteNum(rsEventPo.getVoteNum() + voteNum);
-//    VotePo votePo = VotePo.builder().voteNum(voteNum)
-//            .user(userPo)
-//            .rsEvent(rsEventPo)
-//            .localDateTime(vote.getLocalDateTime())
-//            .build();
-//    rsEventRepository.save(rsEventPo);
-//    userRepository.save(userPo);
-//    voteRepository.save(votePo);
     rsService.vote(vote,eventId);
     return ResponseEntity.ok(null);
   }
--- src/main/java/com/thoughtworks/rslist/service/RsService.java
+++ src/main/java/com/thoughtworks/rslist/service/RsService.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.rslist.service;
 
 
+import com.thoughtworks.rslist.domain.RsEvent;
+import com.thoughtworks.rslist.domain.User;
 import com.thoughtworks.rslist.domain.Vote;
 import com.thoughtworks.rslist.exception.RsEventNotValidException;
 import com.thoughtworks.rslist.po.RsEventPo;
@@ -13,9 +15,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-@Service
 public class RsService {
     final RsEventRepository rsEventRepository;
     final UserRepository userRepository;
@@ -50,5 +53,88 @@ public class RsService {
         rsEventRepository.save(rsEvent);
         userRepository.save(user);
         voteRepository.save(votePo);
+    }
+
+    public RsEvent getEvent(int id) {
+        Optional<RsEventPo> existPo = rsEventRepository.findById(id);
+        if(!existPo.isPresent()){
+            throw new RsEventNotValidException("invalid id");
+        }
+        RsEventPo rsEventPo = existPo.get();
+        RsEvent rsEvent = RsEvent.builder()
+                .userId(rsEventPo.getId())
+                .keyWord(rsEventPo.getKeyWord())
+                .eventName(rsEventPo.getEventName())
+                .rsEventId(rsEventPo.getId())
+                .voteNum(rsEventPo.getVoteNum())
+                .build();
+        return rsEvent;
+    }
+
+    public List<RsEvent> getEvents(Integer start, Integer end) {
+        List<RsEvent> rsEvents = rsEventRepository.findAll().stream().map(
+                item -> RsEvent.builder().keyWord(item.getKeyWord())
+                        .eventName(item.getEventName())
+                        .userId(item.getUserPo().getId()).build()
+        ).collect(Collectors.toList());
+
+
+        if(start == null || end == null){
+            return rsEvents;
+        }
+        if(start<=0 || end > rsEventRepository.findAll().size() || start>end){
+            throw new RsEventNotValidException("invalid request param");
+        }
+        return rsEvents.subList(start-1 , end);
+    }
+
+    public List<User> getUsers() {
+        List<User> users = userRepository.findAll().stream().map(
+                item -> User.builder()
+                        .voteNumber(item.getVoteNumber())
+                        .phone(item.getPhone())
+                        .name(item.getName())
+                        .gender(item.getGender())
+                        .email(item.getEmail())
+                        .age(item.getAge())
+                        .build()
+        ).collect(Collectors.toList());
+        return users;
+    }
+
+    public void postEvent(RsEvent rsEvent) {
+        if(!userRepository.findById(rsEvent.getUserId()).isPresent()){
+            throw new RsEventNotValidException("invalid param");
+        }
+        UserPo userPo = userRepository.findById(rsEvent.getUserId()).get();
+        RsEventPo rsEventPo = RsEventPo
+                .builder()
+                .keyWord(rsEvent.getKeyWord())
+                .eventName(rsEvent.getEventName())
+                .userPo(userPo).build();
+        rsEventRepository.save(rsEventPo);
+    }
+
+    public void patchEvent(int eventId, RsEvent rsEvent) {
+        if(!rsEventRepository.findById(eventId).isPresent()){
+            throw new RsEventNotValidException("invalid param");
+        }
+        int userId = rsEvent.getUserId();
+        RsEventPo rsEventPo = rsEventRepository.findById(eventId).get();
+
+        if(userId!= rsEventPo.getUserPo().getId()){
+            throw new RsEventNotValidException("invalid param");
+        }
+        if(rsEvent.getKeyWord()!=null){
+            rsEventPo.setKeyWord(rsEvent.getKeyWord());
+        }
+        if(rsEvent.getEventName()!=null){
+            rsEventPo.setEventName(rsEvent.getEventName());
+        }
+        rsEventRepository.save(rsEventPo);
+    }
+
+    public void deleteEvent(int id) {
+        rsEventRepository.deleteById(id);
     }
 }
